7bf3b9f6709cca734995975e6fef11ec4067e22b
spacecop <copiosacumlucis@gmail.com>
2022-04-08 04:48:36 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main
+  * main - entry point
   *
   * Return: always 0 (success)
-  8/
+  */
   int main(void)
   {
-  write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
   return (1);
   }
