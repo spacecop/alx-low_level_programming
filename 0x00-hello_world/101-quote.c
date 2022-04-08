c5c6efdeaa4df3c3b17b0f1a984dd0e1521aaae7
spacecop <copiosacumlucis@gmail.com>
2022-04-08 04:36:52 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-write(1, "and that pieace of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(and that pieace of art is useful - Dora Korpar, 2015-10-19);
 return (1);
 }
