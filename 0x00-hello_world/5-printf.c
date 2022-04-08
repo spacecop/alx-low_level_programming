403a8a55c419a058d647a03e77083a182dde8ac1
spacecop <copiosacumlucis@gmail.com>
2022-04-07 21:21:14 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-  main - Entry point
-  *
-REturn: Always 0 (success)
-*/
+ * main - Entry point
+ *
+ * REturn: Always 0 (success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
