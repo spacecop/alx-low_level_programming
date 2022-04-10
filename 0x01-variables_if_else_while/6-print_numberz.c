99be07d1413905653ed342e36dbbb336845e4ed5
spacecop <copiosacumlucis@gmail.com>
2022-04-10 16:51:00 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0 (success/correct)
+ */
+int main(void)
+{
+int i;
+for (i = 48 ; i <= 57; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
