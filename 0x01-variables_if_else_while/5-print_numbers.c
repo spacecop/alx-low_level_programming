5d2a6ae81a82b79dcc46c11274ab4f520590210c
spacecop <copiosacumlucis@gmail.com>
2022-04-10 16:07:00 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0 ; digit < 10; digit++)
+{
+printf("%digit", digit);
+}
+printf("\n");
+return (0);
+}
