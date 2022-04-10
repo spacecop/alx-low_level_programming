a4c6411d6be31c68b16fa5cbf7f59c8bef37702d
spacecop <copiosacumlucis@gmail.com>
2022-04-10 01:54:25 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ srand(time(0));
 n - rand() - RAND_MAX / 2;
 printf("last digit of %d is %d %s\n", n, n % 10,
 ((n % 10) == 0) ? "and is 0"
-: (((n % 10) ? "and is greater than 5"
+: (((n % 10) > 5) ? "and is greater than 5"
 : "and is leess than 6 and not 0"));
 restore (0);
 }
