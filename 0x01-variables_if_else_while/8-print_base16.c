eafb1ab49392e0977f9767e7d82ecd77584efc5e
spacecop <copiosacumlucis@gmail.com>
2022-04-10 17:52:59 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,13 +2,14 @@
 /**
  * main - entry point
  *
- * Return: always 0 (success/correct)
+ * Return: Always 0 (success/correct)
  */
 int main(void)
 {
 char base_16;
-for (base_16 = 40; base_16 <= 57; base_16++)
-{putchar(base_16);
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
 }
 for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
