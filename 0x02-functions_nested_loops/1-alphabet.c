115863c8aabc8c0aee480101b96ec3f538e2e7a8
spacecop <copiosacumlucis@gmail.com>
2022-04-14 03:23:20 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0 successful
+ */
+int main(void)
+{
+char c = 'a';
+{
+while (c <= 'z')
+_putchar(c);
+c++;
+}	       
+_putchar('\n');
+return (0);
+}
