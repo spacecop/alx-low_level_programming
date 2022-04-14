0603df8631f4d2b84874f6623e8422d0f0adb303
spacecop <copiosacumlucis@gmail.com>
2022-04-13 22:52:25 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-char c = 'a';
-while (c <= 'z')
+char alphabet = 'a';
+while (alphabet <= 'z')
 {
-_putchar(c);
-c++;
+_putchar(alphabet);
+alphabet++;
 }
 _putchar('\n');
 return (0);
