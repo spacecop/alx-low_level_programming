5d036da259de33daa32ac99d07a56c05cfbdcbb5
spacecop <copiosacumlucis@gmail.com>
2022-04-15 22:08:03 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,16 +6,19 @@
  */
 int main(void)
 {
-	int i;
-	printf("Infinite loop incoming :(\n");
-	i = 0;
-	/*
-	*while ( i < 10)
-	*{
-	*Infinite loop - No increase of variable
-        *putchar(i);
-        }
-        */
-        printf("Infinite loop avoided! \\o/\n");
-	return (0);
+int i:
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+*while (i < 10)
+*{
+*Infinite loop - No increase of variable
+*putchar(i);
+}
+*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
