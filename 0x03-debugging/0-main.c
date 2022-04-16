c80784d175fd42a0ca10130049473d6ccff456bd
spacecop <copiosacumlucis@gmail.com>
2022-04-15 19:33:15 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,10 +3,14 @@
  * main - Entry point
  * Return: 0
  */
- int main(void)
+
+int main(void)
 {
-	int i;
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+int i;
+i = 0;
+
+positive_or_negative(i);
+
+return (0);
+
 }
