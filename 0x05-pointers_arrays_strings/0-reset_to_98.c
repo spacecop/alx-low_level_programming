92ce49e30cde4806537798dc1a284ea3ac96f6fd
spacecop <copiosacumlucis@gmail.com>
2022-04-20 10:48:14 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates an integer value to 98
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: 0 that means it is correct
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+My 0x05.C -pointers. arraus and strings
