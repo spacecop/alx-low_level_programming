81b9443d30d500366ff38de9f104f9aad94b0afe
spacecop <copiosacumlucis@gmail.com>
2022-04-25 16:34:34 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int x, y;
+int x, y;
 
-	for (x = 0; dest[x] != '\0'; x++);
-	for (y = 0; src[y] != '\0'; y++)
-	{
-		dest[x] = src[y];
-		x++;
-	}
-	dest[x] = '\0';
-	return (dest);
+for (x = 0; dest[x] != '\0'; x++);
+for (y = 0; src[y] != '\0'; y++)
+{
+dest[x] = src[y];
+x++;
+}
+dest[x] = '\0';
+return (dest);
 }
