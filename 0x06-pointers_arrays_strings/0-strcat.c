0a17971355813e6cfac2fb00165c50f5b4c90270
spacecop <copiosacumlucis@gmail.com>
2022-04-25 19:26:54 -0700

    Task 1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,11 @@
  * *_strcat - concatenates two strings
  * @dest: second string to be concatenated to src
  * @src: first string to be concatenated to dest
- *
  * Return: pointer to the resulting string dest
  */
 char *_strcat(char *dest, char *src)
 {
 int x, y;
-
 for (x = 0; dest[x] != '\0'; x++);
 for (y = 0; src[y] != '\0'; y++)
 {
