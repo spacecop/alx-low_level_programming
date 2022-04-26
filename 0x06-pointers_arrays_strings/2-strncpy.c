7345ace1f341ed75723d5b599c7a887b2c276fc8
spacecop <copiosacumlucis@gmail.com>
2022-04-25 19:36:31 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strncpy - function that copies a string
+ * @dest: string to copy to
+ * @arc: string to be copied from
+ * @n: integer that determnes number of bytes to be copied
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+	
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+	fir (; n > x; x++)
+		dest[x] = '\0';
+
+	return (dest);
+}
