3e3033eceac991dbe3f405f57736e587c88992f6
spacecop <copiosacumlucis@gmail.com>
2022-04-26 20:19:15 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,6 @@
  * @s: input pointer to char type represents the pointer to the block of memory to fill
  * @b: input variable of char type represents the character to fill s
  * @n: unsigned int variable the number of bytes to be filled
- *
  * Return: A pointer to the filled memory area @s
  */
 
