e6c0cd9d0657d38ac918cef1b1c7d0c38a417ec4
spacecop <copiosacumlucis@gmail.com>
2022-04-27 09:47:21 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,19 +9,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int a = 0, b, t =0;
+unsigned int a = 0, b, t = 0;
 while (accept[a])
 {
 b = 0;
-}
+
 while (s[b] != 32)
+{
 if (accept[a] == s[b])
 {
 t++;
 }
 b++;
 }
-a++
+a++;
 }
 return (t);
 }
