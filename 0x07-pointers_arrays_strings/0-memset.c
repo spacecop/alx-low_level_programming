19bd07632838321a14aca5fa63766ec1d39314d9
spacecop <copiosacumlucis@gmail.com>
2022-04-26 20:26:05 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * char *_memset - a function that fills memeory with a constant byte
  *
  * @s: input pointer to char type represents the pointer to the block of memory to fill
  * @b: input variable of char type represents the character to fill s
  * @n: unsigned int variable the number of bytes to be filled
+ *
  * Return: A pointer to the filled memory area @s
  */
 
