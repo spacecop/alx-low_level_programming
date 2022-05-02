3398323002fb2106b951d6a14c5021be1be5f719
spacecop <copiosacumlucis@gmail.com>
2022-05-02 03:52:49 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _evaluate(int i, int n)
 if (n == 0 || n == 1)
 return (n);
 else if (i * i < n)
-return (_evaluate(i +1, n));
+return (_evaluate(i + 1, n));
 else if (i * i == n) /*Condition base*/
 return (i);
 return (-1);
