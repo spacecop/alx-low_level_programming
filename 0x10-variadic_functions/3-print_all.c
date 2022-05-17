ad6fefb3d6570783fd2ec6fe7e2b63358f4679f9
spacecop <copiosacumlucis@gmail.com>
2022-05-16 22:29:37 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -27,19 +27,19 @@ void print_all(const char * const format, ...)
 		}
 		switch (format[i])
 		{
-		case 'c':
+		case 'c' :
 			printf("%c", va_arg(valist, int)), c = 1;
 			break;
-		case 'i':
+		case 'i' :
 			printf("%i", va_arg(valist, int)), c = 1;
 			break;
-		case 'f':
+		case 'f' :
 			printf("%f", va_arg(valist, double)), c = 1;
 			break;
-		case 's':
-			str = va_arg(valist, char *), c = 1;
-			if (!str)
-			{
+		case 's' :
+			 str = va_arg(valist, char *), c = 1;
+			 if (!str)
+			{ 
 				printf("(nil)");
 				break;
 			}
