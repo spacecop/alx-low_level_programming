e0508748c60fb8cccd2a078ff0e790ecacf99f2c
spacecop <copiosacumlucis@gmail.com>
2022-05-18 01:32:57 -0700

    Task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -31,7 +31,7 @@ void print_all(const char * const format, ...)
 			printf("%c", va_arg(valist, int)), c = 1;
 			break;
 		case 'i':
-			printf("%i", va_arg(valist, int)), c = 1;
+			printf("%d", va_arg(valist, int)), c = 1;
 			break;
 		case 'f':
 			printf("%f", va_arg(valist, double)), c = 1;
@@ -39,9 +39,9 @@ void print_all(const char * const format, ...)
 		case 's':
 			str = va_arg(valist, char *), c = 1;
 			if (!str)
-			{ 
-			printf("(nil)");
-			break;
+			{
+				printf("(nil)");
+				break;
 			}
 			printf("%s", str);
 			break;
