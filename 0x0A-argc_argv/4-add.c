9c4c4e8c332564cea4ce4d96ea223c19924b763e
spacecop <copiosacumlucis@gmail.com>
2022-05-18 06:23:06 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -50,7 +50,7 @@ int sum = 0;
 count = 1;
 while (count < argc) /*goes through the whole array*/
 {
-if(check_num(argv[count]))
+if (check_num(argv[count]))
 {
 str_to_int = atoi(argv[count]);
 sum += str_to_int;
