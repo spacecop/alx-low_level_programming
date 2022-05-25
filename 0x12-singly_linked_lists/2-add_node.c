93cbe7e5bcdfdfc4acf808badb762886f152f80b
spacecop <copiosacumlucis@gmail.com>
2022-05-25 06:38:32 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the behinnings
+ * add_node - adds a new node at the behinning
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
  * Return: address of the head.
  */
 
-list-t *add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
 size_t nchar;
