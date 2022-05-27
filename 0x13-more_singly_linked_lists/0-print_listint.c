126168e3356e0bdef859bb474dccb69298d21a97
spacecop <copiosacumlucis@gmail.com>
2022-05-27 13:12:50 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,16 +3,19 @@
 /**
  * print_listint - prints all the element of a linked list
  * @h: linked list of type listint_t to print
+ *
  * Return: Number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
 size_t num = 0;
+
 while (h)
 {
 printf("%d\n", h->n);
 num++;
 h = h->next;
 }
+
 return (num);
 }
