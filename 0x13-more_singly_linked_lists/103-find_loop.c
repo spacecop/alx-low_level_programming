677c749eded00443bbf56fccbe801378059e3997
spacecop <copiosacumlucis@gmail.com>
2022-05-28 14:51:56 -0700

    Task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * find_listint_loop - finds the loop in a linked list
- * @head: linked list to search for 
+ * @head: linked list to search for
  *
  * Return: address of the node where the loop starts, or NULL
  */
