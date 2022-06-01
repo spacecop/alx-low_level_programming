976743cd2f2b96d315ef0f80bb3e1881a4c3e4bc
spacecop <copiosacumlucis@gmail.com>
2022-06-01 09:18:03 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	file = open(filename, O_CREATE | O_TRUNC | O_WRONLY, 0600);
 	if (file == -1)
 			return (-1);
 	if (text_content != NULL)
@@ -25,4 +25,5 @@ int create_file(const char *filename, char *text_content)
 	close(file);
 	return (1);
 }
+T ADD .
 9.r  n                                                          
