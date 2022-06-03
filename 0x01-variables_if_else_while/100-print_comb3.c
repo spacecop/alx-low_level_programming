fca54b272b7d99c7cb0801e75c11b87ece3b7838
spacecop <copiosacumlucis@gmail.com>
2022-06-03 07:01:50 -0700

    Advanced

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,14 +15,14 @@ for (number_left = 48; number_right <= 78; number_left++)
 	{
 
 		putchar(number_left);
-		putchar (number_right);
+		putchar(number_right);
 
 		if ((number_left == 56) && (number_right == 78))
 		{
 			break;
 		}
 		putchar(',');
-		putchar (' ');
+		putchar(' ');
 
 	}
 
