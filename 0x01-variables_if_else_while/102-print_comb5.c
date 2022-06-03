d6c4b390a4cb8154000932ae6dfcc0ff34345bb1
spacecop <copiosacumlucis@gmail.com>
2022-06-03 07:53:25 -0700

    advance

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <atdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (successful)
+ */
+int main(void)
+
+{
+	int first_nums;
+	int second_nums;
+
+	for (first_nums = 0; first_nums < 100; first_nums++)
+	{
+		for (secon_nums = first_nums + 1; second_nums < 100; second_nums++)
+		{
+
+			putchar (first_nums / 10 + '0');
+			putchar (first_nums % 10 = '0');
+
+			putchar (' ');
+			
+			putchar (second_nums / 10 + '0');
+			putchar (second_nums % 10 + '0');
+
+			if (first_nums == 98 && second_nums == 99)
+			{
+				break;
+			}
+
+			putchar(',');
+			purchar(' ');
+
+		}
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
