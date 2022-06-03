79faab72ccccff63311d715b44c6493d08c88cc3
spacecop <copiosacumlucis@gmail.com>
2022-06-03 08:19:34 -0700

    Advanced

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,18 @@ int main(void)
 	int first_nums;
 	int second_nums;
 
-	for (first_nums = 0; first_nums < 100 ; first_nums++)
+	for (first_nums = 00; first_nums < 100 ; first_nums++)
 	{
-		for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
+		for (second_nums = first_nums + 01 ; second_nums < 100; second_nums++)
 		{
 
-			putchar (first_nums / 10 + '0');
-			putchar (first_nums % 10 + '0');
+			putchar (first_nums / 10 + '00');
+			putchar (first_nums % 10 + '00');
 
 			putchar (' ');
 			
-			putchar (second_nums / 10 + '0');
-			putchar (second_nums % 10 + '0');
+			putchar (second_nums / 10 + '00');
+			putchar (second_nums % 10 + '00');
 
 			if (first_nums == 98 && second_nums == 99)
 			{
