fd23f18e5180f959a3836116789975236c0f6c17
spacecop <copiosacumlucis@gmail.com>
2022-06-05 07:23:51 -0700

    advanced

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,78 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isprintableASCII - determines if n is a printable ASCII char
+ * @n: integer
+ * Return: 1 if true, 0 if false
+ */
+int isprintableASCII(int n)
+{
+	return (n >= 32 && n <= 126);
+}
+
+/**
+ * printHexes - print hex valuesfor string b in formated form
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printHexes(char *b, int start, int end)
+{
+int i = 0;
+
+while (i < 10)
+{
+	if (i < end)
+		printf("%02x", *(b + start + i));
+	else
+		printf(" ");
+	if (i % 2)
+		printf(" ");
+	i++;
+}
+}
+
+/**
+ * printASCII - print ascii value for string b
+ * formated to replace nonprintable chars with '.'
+ * @b: string to print
+ * @start: starting position
+ * @end: ending position
+ */
+void printASCII(char *b, int start, int end)
+{
+	int ch, i = 0;
+
+	while (i < end)
+	{ch = *(b + i + start);
+		if (!isprintableASCII(ch))
+			ch = 46;
+		printf("%c", ch);
+		i++;
+	}
+}
+
+/*
+ * print_buffer - prints a buffer
+ * @b: string
+ * @size: size of buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int start, end;
+
+	if (size > 0)
+	{
+		for (start = 0; start < size; start += 10)
+		{
+			end = (size - start < 10) ? size - start : 10;
+			printf("%08x: ", start);
+			printHexes(b, start, end);
+			printASCII(b, start, end);
+			printf("\n");
+		}
+		else
+			printf("\n");
+	}
+
