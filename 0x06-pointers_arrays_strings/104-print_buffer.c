eea08900b5ee65f9714739f542f090f4450e958a
spacecop <copiosacumlucis@gmail.com>
2022-06-05 07:46:56 -0700

    advanced

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -45,7 +45,8 @@ void printASCII(char *b, int start, int end)
 	int ch, i = 0;
 
 	while (i < end)
-	{ch = *(b + i + start);
+	{
+		ch = *(b + i + start);
 		if (!isprintableASCII(ch))
 			ch = 46;
 		printf("%c", ch);
