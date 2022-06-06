c0c42e4f4953a715199bdf88cba8d1ebf26cc149
spacecop <copiosacumlucis@gmail.com>
2022-06-06 14:38:39 -0700

    avanced

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,81 @@
+#include "main.h"
+/**
+ * sttow - splits a string into  words
+ * @str: string of words to be split
+ * Return: double pointer to strings
+ */
+char **strtow(char *str)
+{
+	char **ptr;
+	int i, k, len, start, end, j = 0;
+	int words = countWords(str);
+	if (!str || !countWords(str))
+		return (NULL);
+	ptr = malloc(sizeof(char *) * (words + 1));
+
+	if (!ptr)
+		return (NULL);
+	for (i = 0; i < words; i++)
+	{
+		start = startIndex(str, j);
+		end = endIndex(str, start);
+		len = end - start;
+		ptr[i] = malloc(sizeof(char) * (len + 1));
+		if (!ptr[i])
+		{
+			i -= 1;
+			while (i >= 0)
+			{
+				free(ptr[i]);
+				i--;
+			}
+			free(ptr);
+			return (NULL);
+		}
+		for (k = 0; k < len; k++)
+			ptr[i][k] = str[start++];
+		ptr[i][k++] = '\0';
+		j = end + 1;
+	}
+
+	ptr[i] = NULL;
+	return (ptr);
+}
+
+/**
+ * startIndex - returns first index of non-space char
+ * @s: input string
+ * @index: starting index
+ * Return: index of first non-space char
+ */
+int startIndex(char *s, int index)
+{
+
+	while (isSpac(*(s +index)))
+		index++;
+	return (index);
+}
+
+/**
+ * countWords - counts numbers of words in string
+ * @s: input string
+ * REturn: number of words
+ */
+int countWords(char *s)
+{
+	int wordOn = 0;
+	int words = 0;
+
+	while (*s)
+	{
+		if (isSpace(*s) && wordOn)
+			wordOn = 0;
+		else if (!isSpace(*s) && !wordOn)
+		{
+			wordOn = 1;
+			words++;
+		}
+		s++;
+	}
+	return (words);
+}
