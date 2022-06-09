42300812629aa543a1e96f0b6c12c7364f91244f
spacecop <copiosacumlucis@gmail.com>
2022-06-09 01:47:59 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,4 +1,36 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of buffer to print
+ * @size: the size of memory to print
+ *
+ * Return: nothing
+ */
+void simple_print_buffer(char *buffer, unsigned int size)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf(" ");
+		}
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+		printf("0x%02x", buffer[i]);
+		i++;
+	}
+	printf("\n")
+}
+
 
 /**
  * _calloc - allocates memory for nmeb elements of sizebytes
