9a1492bd14661e35ec8d0e99c05c0b09783dd0e6
spacecop <copiosacumlucis@gmail.com>
2022-06-09 02:12:00 -0700

    Task 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,11 +11,11 @@ int *array_range(int min, int max)
 	int *arr, i = 0, t = min;
 	if (min > max)
 		return (0);
-	arr = malloc((max - min + 1) * sizeof(int);
+	arr = malloc((max - min + 1) * sizeof(int));
 
-			if (!arr)
-			return (0);
-			while (i <= max - min)
-			arr[i++] = t++;
-			return (arr);
-			}
+	if (!arr)
+		return (0);
+	while (i <= max - min)
+		arr[i++] = t++;
+	return (arr);
+}
