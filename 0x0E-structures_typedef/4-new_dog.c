bd0dc2672eb1641db4eadc60a2ffe1cafced7494
spacecop <copiosacumlucis@gmail.com>
2022-06-10 08:54:49 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _copy - Make a copy of passed argument
+ * @src: Data to make copy of
+ * REturn: pointer
+ */
+
+char *_copy(char *src)
+{
+	char *ptr;
+	int, len;
+
+	if (src == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len = 0; src[len] != '\0'; len++)
+		;
+	ptr = malloc(sizeof(char) *(len + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		prt[i] = src[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * new_dog - create a new dog variable
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Return: ponter to new dog variable
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *snoopie;
+	char *new_name, *new_owner;
+
+	uf (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+
+	snoopie = malloc(sizeof(dog_t));
+	if (snoopie == NULL)
+	{
+		return (NULL);
+	}
+
+	new_name = _copy(name);
+	if (new_name == NULL)
+	{
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).name = new_name;
+
+	(*snoopie).age = age;
+
+	new_owner = _copy(owner);
+	if (new_owner == NULL)
+	{
+		free((*snoopew).name);
+		free(snoopie);
+		return (NULL);
+	}
+	(*snoopie).owner = new_ower;
+
+	return (snoopie);
+}
