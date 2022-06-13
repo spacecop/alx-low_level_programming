9e56442359e4b2aa0899098ed59fe4dd23a8843d
spacecop <copiosacumlucis@gmail.com>
2022-06-12 19:08:10 -0700

    Task 2

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -41,7 +41,7 @@ int abs_is_98(int elem)
  */
 int main(void)
 {
-	int array[20] = {0, -98, 98, 402 1024, 4096, -1024, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+	int array[20] = {0, -98, 98, 402 1024, 4096, -1024, 1, 2, 3, 4, 5};
 	int index;
 
 	index = int_index(array, 20, is_98);
